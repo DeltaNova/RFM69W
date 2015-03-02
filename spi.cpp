cc0b809525eda48495a645fd0fae2d85fa0d70f6
Matthew Tunstall <git@deltanova.co.uk>
2015-03-02 23:43:13 +0000

    Add spi library file outline

---
+++ spi.cpp
@@ -0,0 +1,4 @@
+// spi.cpp - also see header spi.h
+// Serial Periferal Interface for Atmega328
+// Author: M. Tunstall
+// NOTE: This is heavily commented for my own learning/reference.
---
+++ spi.h
@@ -0,0 +1,15 @@
+// spi.h - Header file for spi.cpp
+// Serial Periferal Interface for Atmega328
+// Author: M. Tunstall
+// NOTE: This is heavily commented for my own learning/reference.
+
+#ifndef SPI_H //If spi.h hasn't been included elsewhere, define it now.
+#define spi.h
+
+class Spi
+{
+    public: // Can be accessed by any function in the program
+    
+    private: // Can only be accessed by functions in this class
+};
+#endif // End of ifndef SPI_H
