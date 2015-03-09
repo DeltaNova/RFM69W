039807e001853af0692dcd682b83f156a6fe9a9e
Matthew Tunstall <git@deltanova.co.uk>
2015-03-09 15:19:43 +0000

    Spi class member functions removed from spi.h. (Moved to spi.cpp)

--- spi.h
+++ spi.h
@@ -49,129 +49,32 @@ class Spi
         void SelectSlave() { PORTB &= ~(1<<PORTB2); }
         void DeselectSlave() { PORTB |= (1<<PORTB2); }
         
-        int8_t SetMode(int8_t Mode);
-        int8_t SetClock(int8_t ClockMode);
-        uint8_t Transiever(uint8_t data);
-};
-
-void Spi::InitSlave() 
-{ 
-    SPCR &= ~(1<<MSTR);
-    //Set SCK,MOSI,SS as inputs
-    DDRB &= ~((1<<DDB5)|(1<<DDB3)|(1<<DDB2));
-    // Set MISO as output
-    DDRB |= (1<<DDB4);
-}
- 
-void Spi::InitMaster() 
-{ 
-    SPCR |= (1<<MSTR);
-    // Set SCK,MOSI,SS as outputs
-    DDRB |= (1<<DDB5)|(1<<DDB3)|(1<<DDB2);
-    // Set MISO as input
-    DDRB &= ~(1<<DDB4);
-    // Set SS line HIGH (slave not enabled)
-    PORTB |= (1<<PORTB2);
-}
- 
-int8_t Spi::SetMode(int8_t Mode)
-{
-    /* 
-       Sets one of the SPI Modes below.
-       Returns 1 if error. 
-       
-       SPI Modes
+        /* 
+        Sets one of the SPI Modes below.
+        Returns 1 if error. 
+        
+        SPI Modes
         0 - CPOL=0, CPHA=0
         1 - CPOL=0, CPHA=1
         2 - CPOL=1, CPHA=0
         3 - CPOL=1, CPHA=1
-    */
-    int8_t return_code = 0;
-    
-    switch(Mode)
-    {
-        case 0:
-            SPCR &= ~((1<<CPOL) | (1<<CPHA));
-            break;
-        case 1:
-            SPCR &= ~(1<<CPOL);
-            SPCR |= (1<<CPHA);
-            break;
-        case 2:
-            SPCR |= (1<<CPOL);
-            SPCR &= ~(1<<CPHA);
-            break;
-        case 3:
-            SPCR |= (1<<CPOL)|(1<<CPHA);
-            break;
-        default:
-            return_code = 1;
-    }
-    return return_code;
-}
-
-int8_t Spi::SetClock(int8_t ClockMode)
-{
-    /*
-     fosc - frequency of crystal/resonator of uC. 
-     
-     Mode - Clock Frequency
-     0          fosc/4 (default)
-     1          fosc/16
-     2          fosc/64
-     3          fosc/128
-     4          fosc/2
-     5          fosc/8
-     6          fosc/32
-     7          fosc/64
-      
-    */
-    int8_t return_code = 0;
-    switch(ClockMode)
-    {
-        case 0: // fosc/4
-            SPSR &= ~(1<<SPI2X);
-            SPCR &= ~((1<<SPR1)|(1<<SPR0));
-        case 1: // fosc/16
-            SPSR &= ~(1<<SPI2X);
-            SPCR &= ~(1<<SPR1);
-            SPCR |= (1<<SPR0);
-        case 2: // fosc/64
-            SPSR &= ~(1<<SPI2X);
-            SPCR |= (1<<SPR1);
-            SPCR &= ~(1<<SPR0);
-        case 3: // fosc/128
-            SPSR &= ~(1<<SPI2X);
-            SPCR |= (1<<SPR1)|(1<<SPR0);
-        case 4: // fosc/2
-            SPSR |= (1<<SPI2X);
-            SPCR &= ~((1<<SPR1)|(1<<SPR0));
-        case 5: // fosc/8
-            SPSR |= (1<<SPI2X);
-            SPCR &= ~(1<<SPR1);
-            SPCR |= (1<<SPR0);
-        case 6: // fosc/32
-            SPSR |= (1<<SPI2X);
-            SPCR |= (1<<SPR1);
-            SPCR &= ~(1<<SPR0);
-        case 7: // fosc/64
-            SPSR |= (1<<SPI2X);
-            SPCR |= (1<<SPR1)|(1<<SPR0);
-        default:
-            return_code = 1;
-    }
-    return return_code;
-}
-
-uint8_t Spi::Transiever(uint8_t data) // Spi member function
-{
-    // Load data into SPDR initiating transfer
-    SPDR = data;
-    //Wait until transmission complete
-    while(!(SPSR)&(1<<SPIF));
-    // Return received data.
-    return(SPDR);
-}
-
+        */
+        int8_t SetMode(int8_t Mode);
+        
+        /*
+        Mode - Clock Frequency  [fosc - freq of uC crystal] 
+        0      fosc/4 (default)
+        1      fosc/16
+        2      fosc/64
+        3      fosc/128
+        4      fosc/2
+        5      fosc/8
+        6      fosc/32
+        7      fosc/64
+        */
+        int8_t SetClock(int8_t ClockMode);
+        
+        uint8_t Transiever(uint8_t data);
+};
 
 #endif // End of ifndef SPI_H
