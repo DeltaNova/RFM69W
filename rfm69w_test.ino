255075a612233573286b0fea2b5135bb27fc29c3
Matthew Tunstall <git@deltanova.co.uk>
2015-03-09 17:46:33 +0000

    Added singleByteWrite to rfm69w_test.ino

--- rfm69w_test.ino
+++ rfm69w_test.ino
@@ -1,6 +1,7 @@
-/*
-    rfm69w_test.ino - An Arduino script to test SPI comms with RFM69W
-*/
+// rfm69w_test.ino - An Arduino script to test SPI comms with RFM69W
+// ---------------------------------------------------------------------
+// Author: M. Tunstall
+// NOTE: This is heavily commented for my own learning/reference.
 
 #include "spi.h" // Include my spi library.
 #include <Wire.h> // Used for serial comms.
@@ -27,21 +28,74 @@ uint8_t singleByteRead(uint8_t byteAddr)
     return readByte;
 }
 
+void singleByteWrite(uint8_t byteAddr, uint8_t dataByte)
+{
+    // Write a single byte to an address over the SPI interface.
+    SPI.SelectSlave();// Slave Select line low
+    // In this instance we are not handling any data the slave returns.
+    // To write, the MSB of the address must be 1; MSB 0 to read.
+    SPI.Transiever(byteAddr|0x80); // Send the address byte.
+    SPI.Transiever(dataByte);   // Send the data byte.
+    SPI.DeselectSlave(); // Slave Select line high
+    return;
+}
 
 void loop()
 {
-        //DEBUG - Print the SPI Registers to Serial Output
+        //Print the SPI Registers to Serial Output
+        Serial.println("SPI Registers");
         Serial.print("SPCR: ");
         Serial.println(SPCR,BIN);
         Serial.print("SPSR: ");
         Serial.println(SPSR,BIN);
-        
-        //End DEBUG
+        delay(1000);
+
+        // SPI - singleByteRead
+        Serial.println("SPI - singleByteRead");
         uint8_t datain = singleByteRead(0x2d);
         Serial.print("ADDR: ");
         Serial.println(0x2d,HEX);
-        Serial.print("Data: ");
+        Serial.println("Expected Data 0x03");
+        Serial.print("Actual Data: ");
         Serial.println(datain,HEX);
         Serial.println();
         delay(1000);
+        
+        // SPI - singleByteWrite
+        Serial.println("SPI - singleByteWrite");
+        singleByteWrite(0x2d,0x04);
+        Serial.print("ADDR: ");
+        Serial.println(0x2d,HEX);
+        Serial.print("Sent Data: ");
+        Serial.println(0x04);
+        Serial.println();
+        delay(1000);
+        
+        // SPI - singleByteRead
+        Serial.println("SPI - singleByteRead");
+        uint8_t datain2 = singleByteRead(0x2d);
+        Serial.print("ADDR: ");
+        Serial.println(0x2d,HEX);
+        Serial.println("Expected Data 0x04");
+        Serial.print("Actual Data: ");
+        Serial.println(datain2,HEX);
+        Serial.println();
+        delay(1000);
+        // SPI - singleByteWrite
+        Serial.println("SPI - singleByteWrite");
+        singleByteWrite(0x2d,0x03);
+        Serial.print("ADDR: ");
+        Serial.println(0x2d,HEX);
+        Serial.print("Sent Data: ");
+        Serial.println(0x03);
+        Serial.println();
+        delay(1000);
+
+
+
+
+
+
+
+
 }
