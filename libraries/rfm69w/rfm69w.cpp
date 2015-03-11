e9f9bf0d7e9f8ce3ec34f172b6d7562c0624ca8a
Matthew Tunstall <git@deltanova.co.uk>
2015-03-11 00:20:14 +0000

    Begin work on rfm69w library

---
+++ libraries/rfm69w/rfm69w.cpp
@@ -0,0 +1,4 @@
+// rfm69w.cpp - Header file for rfm69w.h
+// Library to use the rfm69w transeiver.
+// Author: M. Tunstall
+// NOTE: This is heavily commented for my own learning/reference.
---
+++ libraries/rfm69w/rfm69w.h
@@ -0,0 +1,16 @@
+// rfm69w.h - Header file for rfm69w.cpp
+// Library to use the rfm69w transeiver.
+// Author: M. Tunstall
+// NOTE: This is heavily commented for my own learning/reference.
+#ifndef RFM69W_H // Header guards
+#define rfm69w.h
+
+class RFM69W
+{
+    
+    
+    
+}
+
+
+#endif //RFM69W_H
